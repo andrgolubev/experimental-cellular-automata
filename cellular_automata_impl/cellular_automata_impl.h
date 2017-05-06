d4c30ef238cafd00cec037606cfaf98eddd25791
Andrey Golubev <andpgolubev@gmail.com>
2017-05-07 01:41:56 +0300

    Small refactoring

--- cellular_automata_impl/cellular_automata_impl.h
+++ cellular_automata_impl/cellular_automata_impl.h
@@ -10,9 +10,9 @@ class simple_cellular_automata : public cellular_automata_interface
 {
 public:
     simple_cellular_automata(const int rule_number, const int* initial_cells, const int& cells_amount);
-    virtual void pass_generation();
-    virtual const int return_generated_cells(int* cells_to_return);
-    virtual const int current_generation();
+    void pass_generation() override;
+    const int return_generated_cells(int* cells_to_return) override;
+    const int current_generation() override;
 private:
     std::vector<int> m_cells; /**< cells members can either be 0 or 1 */
     const int m_cells_amount;
